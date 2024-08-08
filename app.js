e2389524ca63d2908be55fa5cdd5c1ee3b18777b
Jabesbyanx <108432086+Jbyanx@users.noreply.github.com>
2024-08-08 16:53:49 -0500

    Agregando funcion desencriptar

--- app.js
+++ app.js
@@ -37,28 +37,29 @@ function encriptar(){
     return textoEncriptado;
 }
 
-function desencriptar(){
-    let textoADesencriptar = document.getElementById("textareaencr").value;
-    let textoDesencriptado = "";
+// Función para desencriptar el texto
+function desencriptar() {
+    let textoEncriptado = document.getElementById("textareaencr").value;
 
-    for(let i=0; i < textoADesencriptar.length; i++){
-        if(textoADesencriptar[i] === "ai")
-            textoDesencriptado += "a";
-        else if(textoADesencriptar[i] === "enter")
-            textoDesencriptado += "e";
-        else if(textoADesencriptar[i] === "imes")
-            textoDesencriptado += "i";
-        else if(textoADesencriptar[i] === "ober")
-            textoDesencriptado += "o";
-        else if(textoADesencriptar[i] === "ufat")
-            textoDesencriptado += "u";
-        else{
-            textoDesencriptado += textoADesencriptar[i];
-        }
+    // Definir las secuencias de encriptación y sus reemplazos
+    const reemplazos = [
+        { clave: "ufat", valor: "u" },
+        { clave: "ober", valor: "o" },
+        { clave: "imes", valor: "i" },
+        { clave: "enter", valor: "e" },
+        { clave: "ai", valor: "a" }
+    ];
+
+    // Reemplazar cada secuencia de encriptación por su letra correspondiente
+    for (const { clave, valor } of reemplazos) {
+        const regex = new RegExp(clave, 'g');
+        textoEncriptado = textoEncriptado.replace(regex, valor);
     }
 
-    return textoDesencriptado;
+    return textoEncriptado;
 }
+
+
 
 document.getElementById("btn-encriptar").addEventListener("click", function(){
     const textareaMensaje = document.getElementById("textarea-mensaje");
@@ -66,6 +67,19 @@ document.getElementById("btn-encriptar").addEventListener("click", function(){
 
     const textoEncriptado = encriptar()
     textareaMensaje.value = textoEncriptado;
+});
+
+document.getElementById("btn-desencriptar").addEventListener("click", function() {
+    // Limpiar el fondo de imagen del textarea en la sección de respuesta
+
+    const textareaMensaje = document.getElementById("textarea-mensaje");
+    textareaMensaje.style.backgroundImage = "";
+
+    // Obtener el texto desencriptado
+    const textoDesencriptado = desencriptar();
+    
+    // Actualizar el textarea en la sección de respuesta con el texto desencriptado
+    textareaMensaje.value = textoDesencriptado;
 });
 
 //boton copiar
